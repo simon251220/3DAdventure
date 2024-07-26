0071c9a4709daf63b17cafc086e43858e18fd6a9
simon251220 <lucca.klein@hotmail.com>
2024-07-26 08:22:01 -0300

    AT3

---
+++ Assets/Scripts/Game Manager/GMStateIntro.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Project.Core.StateMachine;
+
+public class GMStateIntro : StateBase
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public CharacterController characterController; 
+    public float speed = 1f; 
+    public float turnSpeed = 1f; 
+    public float gravity = 9.8f; 
+    private float vSpeed = 0f;
+    public Animator animator;
+    public float jumpSpeed = 15f;
+
+    [Header("Run Setup")] 
+    public KeyCode keyRun = KeyCode.LeftShift; 
+    public float speedRun = 1.5f;
+
+    void Update() 
+    { 
+        transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0); 
+        var inputAxisVertical = Input.GetAxis("Vertical"); 
+        var speedVector = transform.forward * inputAxisVertical * speed;
+
+        if (characterController.isGrounded) 
+        { 
+            vSpeed = 0; 
+            if (Input.GetKeyDown(KeyCode.Space)) 
+            { 
+                vSpeed = jumpSpeed; 
+            } 
+        }
+
+        vSpeed -= gravity * Time.deltaTime; 
+
+
+
+        speedVector.y = vSpeed;
+
+        var isWalking = inputAxisVertical != 0; 
+        if (isWalking) 
+        { 
+            if (Input.GetKey(keyRun)) 
+            { 
+                speedVector *= speedRun; 
+                animator.speed = speedRun; 
+            } 
+            else 
+            { 
+                animator.speed = 1; 
+            } 
+        }
+
+        characterController.Move(speedVector * Time.deltaTime);
+        if (inputAxisVertical != 0) 
+        { 
+            animator.SetBool("Run", true); 
+        } 
+        else 
+        { 
+            animator.SetBool("Run", false); 
+        }
+    }
+}
